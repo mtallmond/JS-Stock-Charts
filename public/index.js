689705f7be5bfe23f9c84aa6647cc480a766055d
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-21 08:45:37 -0500

    use chartjs

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 async function main() {
-
+    console.log(Chart)
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
